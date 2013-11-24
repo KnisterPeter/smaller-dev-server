2390b1205d5b979de8d72f55e740ba0100a4ae65
Markus Wolf <mail@markus-wolf.de>
2013-11-24 11:32:45 +0100

    Wrapped live-reload script in closure

--- src/main/resources/live-reload.js
+++ src/main/resources/live-reload.js
@@ -1,8 +1,10 @@
-var __socket = new WebSocket(location.href.replace(/https?:\/\/([^/]+)\/.*/, 'ws://$1/live-reload'), 'live-reload');
-__socket.onmessage = function(e) {
-  if (e.data === 'reload') {
-    console.log('reloading window', e);
-    location.reload();
+(function() {
+  var __socket = new WebSocket(location.href.replace(/https?:\/\/([^/]+)\/.*/, 'ws://$1/live-reload'), 'live-reload');
+  __socket.onmessage = function(e) {
+    if (e.data === 'reload') {
+      console.log('reloading window', e);
+      location.reload();
+    }
+    if (e.data === 'ping') __socket.send('pong');
   }
-  if (e.data === 'ping') __socket.send('pong');
-}
+})();
